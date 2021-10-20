cc9b1b0f2d542d7d9c7fd71170418777b763dfeb
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-10-20 20:02:22 +0500

    slide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,8 +5,8 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  background-color: coral;
 `;
 
 const Arrow = styled.div`
@@ -16,7 +16,7 @@ const Arrow = styled.div`
   border-radius: 50%;
   display: flex;
   align-items: center;
-  justify-content-center;
+  justify-content: center;
   position: absolute;
   top: 0;
   bottom: 0;
@@ -24,13 +24,67 @@ const Arrow = styled.div`
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
   cursor: pointer;
+  opacity: 0.5;
 `;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+`;
+
 const Slider = () => {
   return (
     <Container>
       <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://images.pexels.com/photos/9145093/pexels-photo-9145093.jpeg?cs=srgb&dl=pexels-%D0%B2%D0%BB%D0%B0%D0%B4%D0%B8%D1%81%D0%BB%D0%B0%D0%B2-%D1%84%D0%B8%D0%BB%D0%B8%D0%BF%D0%BF%D0%BE%D0%B2-9145093.jpg&fm=jpg" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>New paraxod</Title>
+            <Desc>This is new sale for year</Desc>
+            <Button>Shop now!</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
