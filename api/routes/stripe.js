d55f2f32944a990fb05965858c8243727af823d0
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-10-27 15:32:24 +0500

    orders done and stripe keydone

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,18 +1,24 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(
+  "sk_test_51IspusCBAU79yaYIIut6Ozz8LQmql9dvDOWRSXOCrnpd1llJfFHIvxsH99sj6CeXB0HQ4Ug3N3zOCZWk5qZvQKMV0091dugmpX"
+);
+// (process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
- stripe.charges.create({
-  source: req.body.tokenId,
-  amount: req.body.amount,
-  currency: "usd"
- }, (stripeErr, stripeRes) => {
-  if (stripeErr) {
-   res.status(500).json(stripeErr);
-  } else {
-   res.status(200).json(stripeRes)
-  }
- })
-})
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
 
 module.exports = router;
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,14 +1,14 @@
-import { Add, Remove } from '@material-ui/icons';
-import { useSelector } from 'react-redux';
-import styled from 'styled-components';
-import Announcement from '../components/Announcement';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-import { mobile } from '../responsive';
-import StripeCheckout from 'react-stripe-checkout';
-import { useEffect, useState } from 'react';
-import { userRequest } from '../requestMethods';
-import { useHistory } from 'react-router';
+import { Add, Remove } from "@material-ui/icons";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
+import StripeCheckout from "react-stripe-checkout";
+import { useEffect, useState } from "react";
+import { userRequest } from "../requestMethods";
+import { useHistory } from "react-router";
 
 const KEY = process.env.REACT_APP_STRIPE;
 
@@ -16,7 +16,7 @@ const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 20px;
-  ${mobile({ padding: '10px' })}
+  ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
@@ -35,14 +35,14 @@ const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
-  border: ${(props) => props.type === 'filled' && 'none'};
+  border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) =>
-    props.type === 'filled' ? 'black' : 'transparent'};
-  color: ${(props) => props.type === 'filled' && 'white'};
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
 `;
 
 const TopTexts = styled.div`
-  ${mobile({ display: 'none' })}
+  ${mobile({ display: "none" })}
 `;
 const TopText = styled.span`
   text-decoration: underline;
@@ -53,7 +53,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
-  ${mobile({ flexDirection: 'column' })}
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const Info = styled.div`
@@ -63,7 +63,7 @@ const Info = styled.div`
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
-  ${mobile({ flexDirection: 'column' })}
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const ProductDetail = styled.div`
@@ -112,13 +112,13 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
-  ${mobile({ margin: '5px 15px' })}
+  ${mobile({ margin: "5px 15px" })}
 `;
 
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
-  ${mobile({ marginBottom: '20px' })}
+  ${mobile({ marginBottom: "20px" })}
 `;
 
 const Hr = styled.hr`
@@ -143,8 +143,8 @@ const SummaryItem = styled.div`
   margin: 30px 0px;
   display: flex;
   justify-content: space-between;
-  font-weight: ${(props) => props.type === 'total' && '500'};
-  font-size: ${(props) => props.type === 'total' && '24px'};
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
 `;
 
 const SummaryItemText = styled.span``;
@@ -167,15 +167,18 @@ const Cart = () => {
   const onToken = (token) => {
     setStripeToken(token);
   };
+  console.log(stripeToken)
 
   useEffect(() => {
     const makeRequest = async () => {
       try {
-        const res = await userRequest.post('/checkout/payment', {
+        const res = await userRequest.post("/checkout/payment", {
           tokenId: stripeToken.id,
           amount: 500,
         });
-        history.push('/success', { data: res.data });
+        history.push("/success", {
+          stripeData: res.data,
+          products: cart, });
       } catch {}
     };
     stripeToken && makeRequest();
