d5bdfd061c6e966155432b503b408a5b6001a0ab
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-10-28 15:57:25 +0500

    redux/persist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
+import { useSelector } from "react-redux";
+import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import Success from "./pages/Success";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import Success from "./pages/Success";
+
 const App = () => {
-  const user = false;
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <Router>
       <Switch>
