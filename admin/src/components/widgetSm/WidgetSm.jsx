b8a4df1502fed086c40fdd38b26d7ac103236fb0
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-10-29 23:12:49 +0500

    git added widget

---
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,87 @@
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New join members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/899757/pexels-photo-899757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/899757/pexels-photo-899757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/899757/pexels-photo-899757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/899757/pexels-photo-899757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/899757/pexels-photo-899757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
